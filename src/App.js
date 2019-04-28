e130511bb444f9155af9eae3c760ccbd153a8b97
gaohan1994 <871418277@qq.com>
2019-04-28 21:12:29 +0800

    production

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import logo from './img/logo.png';
+// import eosimg from './img/img_jifen.png';
 
 class App extends Component {
+
+  onIOSClickHandle = () => {
+    window.location.href = 'itms-services:///?action=download-manifest&url=https://net.huanmusic.com/eos/app/01251404/cceos.plist';
+  }
+
+  onAndroidClickHandle = () => {
+    window.location.href = 'https://net.huanmusic.com/eos/app/04041423/cceos.apk';
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="wrapper">
+          {/** 
+          <img className="eos-logo" alt="logo" src="//net.huanmusic.com/eos/lanch/logo.png"/>
+          <img className="eos-img" alt="eos-img" src="//net.huanmusic.com/eos/lanch/img_jifen.png" />
+          */}
+          <div className="eos-logo" alt="logo" />
+          
+          <span className="eos-title">升腾雨滴</span>
+          <div onClick={this.onIOSClickHandle}  className="eos-button eos-ios" />
+          <div onClick={this.onAndroidClickHandle} className="eos-button eos-android" />
+          <div className="eos-img" alt="eos-img" />
+        </div>
       </div>
     );
   }
