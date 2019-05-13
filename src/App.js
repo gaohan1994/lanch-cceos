9b4d79f11da8c31e4258df3e0d2085e3642ee196
gaohan1994 <871418277@qq.com>
2019-05-13 19:37:25 +0800

    new production

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-// import logo from './img/logo.png';
-// import eosimg from './img/img_jifen.png';
 
 class App extends Component {
 
   onIOSClickHandle = () => {
-    window.location.href = 'itms-services:///?action=download-manifest&url=https://net.huanmusic.com/eos/app/01251404/cceos.plist';
+    window.location.href = 'itms-services:///?action=download-manifest&url=https://net.huanmusic.com/eos/production/05131931/cceos.plist';
   }
 
   onAndroidClickHandle = () => {
-    window.location.href = 'https://net.huanmusic.com/eos/app/04041423/cceos.apk';
+    window.location.href = 'https://net.huanmusic.com/eos/production/05131931/cceos-release.apk';
   }
 
   render() {
     return (
       <div className="App">
         <div className="wrapper">
-          {/** 
-          <img className="eos-logo" alt="logo" src="//net.huanmusic.com/eos/lanch/logo.png"/>
-          <img className="eos-img" alt="eos-img" src="//net.huanmusic.com/eos/lanch/img_jifen.png" />
-          */}
           <div className="eos-logo" alt="logo" />
           
           <span className="eos-title">升腾雨滴</span>
